b029337e6d7f95958cc3e2eaca5e5edcde519b1b
Shivam Kalra <dev@kaltq.com>
2024-05-01 16:41:50 +0530

    fix: key warning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,15 +12,14 @@ export default function Home() {
       <nav className="my-16 animate-fade-in">
         <ul className="flex items-center justify-center gap-4">
           {navigation.map((item) => {
-            return <>
+            return <React.Fragment key={item.href}>
             <Link
-              key={item.href}
               href={item.href}
               className="text-sm duration-500 text-zinc-500 hover:text-zinc-300">
               {item.name}
             </Link>
             {item.name === "Contact" && <BookCallBtn />}
-            </>
+            </React.Fragment>
           })}
         </ul>
       </nav>
