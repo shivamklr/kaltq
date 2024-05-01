bab990c8855ac53755b98c0d3150ff03adf783a2
Shivam Kalra <dev@kaltq.com>
2024-05-01 15:42:13 +0530

    feat: add particles in the bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import React from "react";
 import { BookCallBtn } from "./components/bookcall";
+import Particles from "./components/particles";
 const navigation = [
   { name: "Contact", href: "/contact" },
 ];
@@ -24,7 +25,10 @@ export default function Home() {
         </ul>
       </nav>
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-      
+      <Particles
+        className="absolute inset-0 -z-10 animate-fade-in"
+        quantity={100}
+      />
       <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
         Kal Tech
       </h1>
