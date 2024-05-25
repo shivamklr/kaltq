fd62b9d62f2febffd7b17145f6977075060231a6
Shivam Kalra <dev@kaltq.com>
2024-05-26 01:49:20 +0530

    fix: nit

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,18 @@ const nextConfig = {
     output:"export",
     reactStrictMode:true,
     basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+    async rewrites() {
+        return [
+            {
+                source: '/support',
+                destination: '/terms-of-service',
+            },
+            {
+                source: '/cancellation',
+                destination: '/terms-of-service',
+            },
+        ];
+    }
 };
 
 export default nextConfig;
